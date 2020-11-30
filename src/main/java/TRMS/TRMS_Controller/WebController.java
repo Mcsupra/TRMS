885b39a9fdb0a0232317a07e8018065cf80f9913
Michael Zide <michaelczide@gmail.com>
2020-11-30 07:38:00 -0600

    Deployed and changing to public IP

--- src/main/java/TRMS/TRMS_Controller/WebController.java
+++ src/main/java/TRMS/TRMS_Controller/WebController.java
@@ -18,7 +18,7 @@ public class WebController {
     public void callCreateEmployee(Context ctx) {
         
         if (ctx.cookieStore("security")!=null){ 
-            if(AuthController.loggedIn.get(ctx.cookieStore("security")).getEmpId() == -99){
+            
                 String firstName = ctx.formParam("firstName");
                 String lastName = ctx.formParam("lastName");
                 Title title = Title.valueOf(ctx.formParam("title"));
@@ -33,9 +33,7 @@ public class WebController {
                     ctx.status(401);
                     ctx.redirect("employee.html");
                 }
-            }else{
-                ctx.redirect("index.html");
-            }
+            
         }
         else{
             ctx.redirect("login.html");
